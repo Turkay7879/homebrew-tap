98f884282e95695e558976db606c8ef5935b05ba
Türkay <54273457+Turkay7879@users.noreply.github.com>
2026-05-24 23:19:13 +0300

    Add IntelliJ Idea Ultimate 2023.3.2

---
+++ Casks/intellij-idea@2023.3.2.rb
@@ -0,0 +1,45 @@
+cask "intellij-idea@2023.3.2" do
+  arch arm: "-aarch64"
+
+  version "2023.3.2,233.13135.103"
+  sha256 arm:   "0e47cdd338790bdfc7eb0c70feb1ba962e4cda115eb39f074dd2267e525caa12",
+         intel: "a08038442c3f5f60b0890a42ada905bc08928ec070bbfac075c07259ddf6518c"
+
+  url "https://download.jetbrains.com/idea/ideaIU-#{version.csv.first}#{arch}.dmg"
+  name "IntelliJ IDEA Ultimate"
+  desc "Java IDE by JetBrains"
+  homepage "https://www.jetbrains.com/idea/"
+
+  livecheck do
+    url "https://data.services.jetbrains.com/products/releases?code=IIU&latest=true&type=release"
+    strategy :json do |json|
+      json["IIU"].map do |release|
+        "#{release["version"]},#{release["build"]}"
+      end
+    end
+  end
+
+  auto_updates true
+  depends_on macos: ">= :high_sierra"
+
+  app "IntelliJ IDEA.app"
+
+  uninstall_postflight do
+    ENV["PATH"].split(File::PATH_SEPARATOR).map { |path| File.join(path, "idea") }.each do |path|
+      if File.readable?(path) &&
+         File.readlines(path).grep(/# see com.intellij.idea.SocketLock for the server side of this interface/).any?
+        File.delete(path)
+      end
+    end
+  end
+
+  zap trash: [
+    "~/Library/Application Support/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Caches/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Logs/JetBrains/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/com.jetbrains.intellij.plist",
+    "~/Library/Preferences/IntelliJIdea#{version.major_minor}",
+    "~/Library/Preferences/jetbrains.idea.*.plist",
+    "~/Library/Saved Application State/com.jetbrains.intellij.savedState",
+  ]
+end
